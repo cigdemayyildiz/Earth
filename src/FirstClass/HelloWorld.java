5648418c4428834eb8a8035e4381dbfe92d9be1c
Cigdem <cigdemrespect@gmail.com>
2021-09-04 19:14:20 -0500

    first commit

---
+++ src/FirstClass/HelloWorld.java
@@ -0,0 +1,17 @@
+package FirstClass;
+
+public class HelloWorld {
+
+    // This is called main method which like an engine of the car
+    public static void main(String[] args) {
+        // scope: in between two curly braces are called scope
+        // everything in between double quotation mark ("") is String
+        // This is single comment
+        /*
+           This is multi line comment
+         */
+        System.out.println("Hello World!");
+        System.out.println("Hello Cigdem!");
+        System.out.println("easy to learn Java!");
+    }
+}
