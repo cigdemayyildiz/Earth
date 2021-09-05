9e695c8a20de5e3c87af632fd6fd75732c46542d
Cigdem <cigdemrespect@gmail.com>
2021-09-04 19:35:08 -0500

    Byte

---
+++ src/PrimitiveDataTypes/Byte.java
@@ -0,0 +1,17 @@
+package PrimitiveDataTypes;
+
+public class Byte {
+
+    public static void main(String[] args) {
+
+        // byte can hold values from -128 to 127
+        byte num1 = 127;
+        byte num2 = -128;
+        byte num3 = 12;
+        // byte num3 = 128; this is too big for byte
+
+        System.out.println(num1); // 127
+        System.out.println(num2); // -128
+        System.out.println(num3); // 12
+    }
+}
