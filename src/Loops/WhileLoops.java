cec21e724cad993c9d2832fa1d0c7bbb483d3af4
Cigdem <cigdemrespect@gmail.com>
2021-09-05 11:04:28 -0500

    While Loops

---
+++ src/Loops/WhileLoops.java
@@ -0,0 +1,15 @@
+package Loops;
+
+public class WhileLoops {
+
+    public static void main(String[] args) {
+
+        int number = 0;
+
+        while (number<10){
+            System.out.println("I want to go home --> " + number);
+            number++;
+        }
+
+    }
+}
