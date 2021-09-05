72f6cd823f3b3b3ce0df3ecb9a6c4b70c615547a
Cigdem <cigdemrespect@gmail.com>
2021-09-04 19:40:41 -0500

    Integer

---
+++ src/PrimitiveDataTypes/Integer.java
@@ -0,0 +1,20 @@
+package PrimitiveDataTypes;
+
+public class Integer {
+
+    public static void main(String[] args) {
+
+        // int;
+
+        int num1 = 127;
+        int num2 = -128;
+        int num3 = 12;
+        int num4 = 450;
+        int num5 = 3200;
+        num5 = num1;
+        int num7 = num1+num5;
+
+        System.out.println(num7); // 254
+
+    }
+}
