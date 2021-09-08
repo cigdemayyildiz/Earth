ec8fbfd420134c11ae526be4e254e9e8665c1877
Cigdem <cigdemrespect@gmail.com>
2021-09-08 18:39:56 -0500

    For Loop

--- src/Loops/ForLoop.java
+++ src/Loops/ForLoop.java
@@ -16,6 +16,19 @@ public class ForLoop {
         for ( ; number<=10 ;number++ ){
             System.out.println(number);
 
+             /*
+            pseudo code
+            1. create a new class
+            2. write the main code
+            3. create a scanner code to get a number from user
+            4. create a sout to ask the number from user
+            5. create an int to store the number of user
+            6. create the for loop line and put a condition in it as like number <=10
+            7. after condition create your break in the for loop parentheses
+            8. create another sout in the for loop body to print the result
+            9. and run the code and make sure if it works or not
+             */
+
         }
     }
 }
