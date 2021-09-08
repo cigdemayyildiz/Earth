bb69effd32f7e1c7faaa3ef4970359c23148b2b5
Cigdem <cigdemrespect@gmail.com>
2021-09-08 18:14:21 -0500

    Do While2

---
+++ src/Loops/DoWhileLoops2.java
@@ -0,0 +1,31 @@
+package Loops;
+
+public class DoWhileLoops2 {
+
+    public static void main(String[] args) {
+
+        int a = 15;
+        int b = 15;
+
+        do {
+            System.out.println("This is DO WHILE example");
+
+        }while (a<b);
+
+
+        do {
+            System.out.println("This is second DO WHILE example");
+            a++;
+        }while (a<=b);
+
+
+        int x = 3;
+        int y = 6;
+
+        do {
+            System.out.println("This is third DO WHILE example");
+            x++;
+        }while (x<y);
+
+    }
+}
