c442719cef17e1969588e4eedcbb23526b4a9e02
Cigdem <cigdemrespect@gmail.com>
2021-09-08 18:12:23 -0500

    Do While

---
+++ src/Loops/DoWhileLoops.java
@@ -0,0 +1,28 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class DoWhileLoops {
+
+    public static void main(String[] args) {
+
+          /*
+        ask user to give a number
+        create a multiplication table from 1 to 10 from that given number
+         */
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter any int number:");
+        int number = input.nextInt();
+
+        int multiplier = 1;
+
+        if (number >= 0) {
+            while (multiplier <= 10) {
+                System.out.println(number + " x " + multiplier + " = " + number * multiplier);
+                multiplier++;
+            }
+        }
+
+    }
+}
