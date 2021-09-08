e575b5bc87a27fa311d687ebcce4f5d159e24225
Cigdem <cigdemrespect@gmail.com>
2021-09-08 18:26:34 -0500

    For Loop2

---
+++ src/Loops/ForLoops2.java
@@ -0,0 +1,22 @@
+package Loops;
+
+public class ForLoops2 {
+
+    public static void main(String[] args) {
+
+
+        for (int number = 1; number<=5 ;number++ ){ // int number = 1; --> local variable. it cannot effect other variables.
+            System.out.println("It is for LOOP " + number);
+        }
+
+        int number = 1; // global variable
+        for (; number<=5 ;number++ ){
+            System.out.println("It is second for LOOP " + number);
+        }
+
+        int numbers;
+        for (numbers = 1; numbers<=5 ;numbers++ ){
+            System.out.println("It is third for LOOP " + numbers);
+        }
+    }
+}
