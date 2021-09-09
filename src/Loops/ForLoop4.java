cac861d1d0ca23c257446f4efa50a2df65eaa020
Cigdem <cigdemrespect@gmail.com>
2021-09-08 20:07:50 -0500

    For Loop4

---
+++ src/Loops/ForLoop4.java
@@ -0,0 +1,26 @@
+package Loops;
+
+public class ForLoop4 {
+
+    public static void main(String[] args) {
+
+        // print out alphabet in uppercase letters
+
+        for (char ch = 'A'; ch<='Z'; ch++) {
+            System.out.print(ch + ", ");
+        }
+
+        // print out alphabet in lowercase letters
+
+        for (char ch = 'a'; ch <= 'z'; ch++){
+            System.out.print(ch + ", ");
+        }
+
+        // print out alphabet in uppercase and lowercase letters
+
+        for (char ch = 'A', ch1 = 'a'; ch<='Z' && ch1<='z'; ch++, ch1++){
+            System.out.println("\n" + ch + ", ");
+            System.out.println(ch1 + ", ");
+        }
+    }
+}
