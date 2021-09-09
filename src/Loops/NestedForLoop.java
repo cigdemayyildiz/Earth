2f135df185fdfd17dc4303c27bf1b48906584698
Cigdem <cigdemrespect@gmail.com>
2021-09-08 20:27:20 -0500

    Nested For Loop

---
+++ src/Loops/NestedForLoop.java
@@ -0,0 +1,16 @@
+package Loops;
+
+public class NestedForLoop {
+
+    public static void main(String[] args) {
+
+        for (int a = 0; a < 3 ; a++){
+
+            for (int b = 0; b < 5; b++){
+
+                System.out.println("a --> " + a + " b --> " + b);
+            }
+        }
+
+    }
+}
