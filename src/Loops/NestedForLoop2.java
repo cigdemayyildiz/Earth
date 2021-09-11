ef1b5419c360902cad35983058a4fa5639975741
Cigdem <cigdemrespect@gmail.com>
2021-09-11 10:49:04 -0500

    Nested For Loop2

---
+++ src/Loops/NestedForLoop2.java
@@ -0,0 +1,13 @@
+package Loops;
+
+public class NestedForLoop2 {
+
+    public static void main(String[] args) {
+
+        for (int hours = 0; hours<12 ;hours++){
+
+            for (int minutes = 0; minutes<=59; minutes++)
+                System.out.println(hours+ ":" + minutes);
+        }
+    }
+}
