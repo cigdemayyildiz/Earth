76e834c9aded3e469ac7c1ba86d4c1f94409f98e
Cigdem <cigdemrespect@gmail.com>
2021-09-11 10:52:59 -0500

    Else Practice

---
+++ src/IfStatements/ElsePractice.java
@@ -0,0 +1,25 @@
+package IfStatements;
+
+import java.util.Scanner;
+
+public class ElsePractice {
+
+    public static void main(String[] args) {
+
+          /* Ask user if they have driver license
+        if they have print "You are great"
+        if they do not hove print "You should have license before getting on the road."
+         */
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Do you have driver license? Yes/No");
+        String driverLicence = input.nextLine();
+
+        if (driverLicence.equalsIgnoreCase("yes")){
+            System.out.println("You are great!"); // if they say "Yes" or "YES" or "YEs" or "yes" result going to be "You are great!"
+        }
+        else { // if they say nothing or except yes result going to be "You should have license before getting on the road."
+            System.out.println("You should have license before getting on the road.");
+        }
+    }
+}
