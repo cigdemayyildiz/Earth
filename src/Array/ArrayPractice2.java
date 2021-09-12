a1845f0972f62d82aa4516c5f48f39a8e619c55b
Cigdem <cigdemrespect@gmail.com>
2021-09-12 12:55:48 -0500

    Array2

---
+++ src/Array/ArrayPractice2.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class ArrayPractice2 {
+
+    public static void main(String[] args) {
+
+        // create an array to store these numbers: 45, 5, 6, 47, 57, 8, 12,30
+
+        int [] numbers = {45, 5, 6, 47, 57, 8, 12,30};
+
+        for (int i=0;i<numbers.length;i++){
+            if (numbers[i]>10){
+                System.out.println(numbers[i]);
+            }
+        }
+    }
+}
