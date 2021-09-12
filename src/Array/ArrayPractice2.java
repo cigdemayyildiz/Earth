2649e76ba3bcc87dd87d5b7c6d8dd5f0f3d7b19d
Cigdem <cigdemrespect@gmail.com>
2021-09-12 12:57:39 -0500

    Array Practice2

--- src/Array/ArrayPractice2.java
+++ src/Array/ArrayPractice2.java
@@ -4,7 +4,7 @@ public class ArrayPractice2 {
 
     public static void main(String[] args) {
 
-        // create an array to store these numbers: 45, 5, 6, 47, 57, 8, 12,30
+        // create an array to store these numbers: 45, 5, 6, 47, 57, 8, 12, 30
 
         int [] numbers = {45, 5, 6, 47, 57, 8, 12,30};
 
