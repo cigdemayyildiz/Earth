412936ada5d41b83c5cc3101b26aa162b933fe11
Cigdem <cigdemrespect@gmail.com>
2021-09-16 09:02:59 -0500

    String

--- src/String/String.java
+++ src/String/String.java
@@ -1,12 +1,12 @@
 package String;
 
-public class StringPractice {
+public class String {
 
-    public static void main(String[] args) {
+    public static void main(java.lang.String[] args) {
 
-        String name = "Techtorial"; // first way of creating String. You should use " ".
+        java.lang.String name = "Techtorial"; // first way of creating String. You should use " ".
 
-        String lastName = new String("Academy"); // second way of creating String
+        java.lang.String lastName = new java.lang.String("Academy"); // second way of creating String
 
         System.out.println(name);
         System.out.println(lastName);
@@ -20,12 +20,12 @@ public class StringPractice {
         System.out.println(name + 10 * 8);
         System.out.println(name + (10 - 8)); 
 
-        String age = " 20";
+        java.lang.String age = " 20";
         int num = 5;
 
         System.out.println(num+age);
 
-        String number = "1";
+        java.lang.String number = "1";
         number += "2";
         number += 2;
         System.out.println(number);
