4a913fed5de3016e99d07cc0214f857df613320f
Cigdem <cigdemrespect@gmail.com>
2021-09-18 10:14:28 -0500

    Practice

--- src/Loops/Practice03.java
+++ src/Loops/Practice03.java
@@ -7,7 +7,8 @@ public class Practice03 {
     public static void main(String[] args) {
 
         /*
-        Using scanner ask user to provide the string value. Print "dad" if the given string contains the "dad", but where the middle 'a' char can be any char.
+        Using scanner ask user to provide the string value. Print "dad" if the given string contains the "dad",
+        but where the middle 'a' char can be any char.
         Example:
         Input the year: testdadtest Output :dad
         Input the year: testd7dtest Output :dad
---
+++ src/Loops/Practice04.java
@@ -0,0 +1,19 @@
+package Loops;
+
+public class Practice04 {
+
+    public static void main(String[] args) {
+
+        /*
+        Using the scanner ask the user to provide String value. Then replace all the char of the String
+        with 'a' using loop and print in every step.
+        For example: Input: Success
+        Output: "auccess"
+        "aaccess"
+        “aaacess”
+        ...
+        “aaaaaaa”
+         */
+
+    }
+}
