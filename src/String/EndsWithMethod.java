63f6523726f7b5e2761b8c97eef48722b9371920
Cigdem <cigdemrespect@gmail.com>
2021-09-21 16:43:29 -0500

    EndsWith Method

---
+++ src/String/EndsWithMethod.java
@@ -0,0 +1,45 @@
+package String;
+
+import java.util.Scanner;
+
+public class EndsWithMethod {
+
+    public static void main(String[] args) {
+
+        // endsWith(); return true or false
+
+        java.lang.String school = "Techtorial Academy";
+
+        System.out.println(school.endsWith("demy")); // true
+        System.out.println(school.endsWith("Techtorial Academy")); // true
+        System.out.println(school.endsWith("rial")); // false
+        System.out.println(school.endsWith("")); // true
+        System.out.println(school.endsWith(" ")); // false
+
+        /*
+        Task
+        By using scanner ask user to provide his/her gender, full name and birth year in the following format
+         F - Jenny Brown - 1999
+         - Check if the input String is starting with F
+         - Check if the input String is starting with M
+         - Check if the input String is containing your name
+         - Check if the input String is ending with birth year
+         */
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter your gender");
+        java.lang.String gender = input.nextLine(); // F
+        System.out.println("Please enter your full name");
+        java.lang.String fullName = input.nextLine(); // Cigdem Ayyildiz
+        System.out.println("Please enter birth year");
+        java.lang.String birthYear = input.next(); // 1988
+        java.lang.String format = (gender + " - " + fullName + " - " + birthYear);
+        System.out.println(format); // F - Cigdem Ayyildiz - 1988
+        System.out.println(format.startsWith("F")); // true
+        System.out.println(format.startsWith("M")); // false
+        System.out.println(format.contains("Cigdem")); // true
+        System.out.println(format.endsWith("1983")); // true
+
+    }
+}
