22712049d5fdc3fc5bae61521dfd5b3724fa65a9
Cigdem <cigdemrespect@gmail.com>
2021-09-22 15:11:21 -0500

    ToUpperCase Method

--- src/String/ToUpperCaseMethod.java
+++ src/String/ToUpperCaseMethod.java
@@ -4,7 +4,22 @@ public class ToUpperCaseMethod {
 
     public static void main(String[] args) {
 
+        // toUpperCase();
 
+        java.lang.String str = "Do what ever it takes ";
+        java.lang.String quote = "JUst dO iT";
+
+        System.out.println(str.toLowerCase());
+        System.out.println(str);
+        System.out.println(quote.toUpperCase());
+        System.out.println(quote);
+        quote = quote.toUpperCase(); // tum harfleri buyuk harf yap metodu
+        System.out.println(quote.indexOf('D')); // bir ust satirda tum harfler buyuk harf yapildigi icin bu metod calisiyor, eger tum harfleri buyuk yapmasaydik sonuc -1 olacakti
+
+        java.lang.String result = str.concat(quote);
+        System.out.println(result);
+        result = result.toUpperCase();
+        System.out.println(result);
 
     }
 }
