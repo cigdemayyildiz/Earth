4da08512afd24c494bee1514b0c062918b2e38a0
Cigdem <cigdemrespect@gmail.com>
2021-09-22 15:15:09 -0500

    Substring Method

--- src/String/SubstringMethod.java
+++ src/String/SubstringMethod.java
@@ -8,11 +8,11 @@ public class SubstringMethod {
 
         java.lang.String string = "computer";
 
-        java.lang.String substring1 = string.substring(5); // it will return 'ter' index number a gore sayiyor ve kelimenin senin verdigin numaradan sonrasini aliyor
+        java.lang.String substring1 = string.substring(5); // it will return 'ter'
 
         System.out.println(substring1);
 
-        java.lang.String part = string.substring(0, 2); // sifir index numarasindan basliyor kelimenin 2 index numarasina kadar olan kismini aliyor, 2 yi almiyor.
+        java.lang.String part = string.substring(0, 2);
         System.out.println(part); // co
 
         java.lang.String email = "    Techtorial@gmail.com    "; // zak@gmail.com     abcd@gmail.com
@@ -20,7 +20,7 @@ public class SubstringMethod {
         java.lang.String part1 = email.substring(email.indexOf("@"));
         System.out.println(part1);
 
-        // method chaining --> bir satirda birden fazla metodu calistirma, eger istenen sonuc sayi olacaksa primitive data type kullaniyoruz ve sayi ile ilgili olan komutu en sona yaziyoruz cunku istenen sonuc o olacak.
+        // method chaining
 
         int indexOfSlash = part1.toUpperCase().concat("/tech").indexOf('/');
         System.out.println(indexOfSlash);
