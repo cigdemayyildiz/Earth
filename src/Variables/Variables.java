00444d7e1cce77eb538c74a58e6939d3bb93a417
Cigdem <cigdemrespect@gmail.com>
2021-09-29 16:58:39 -0500

    Variables

---
+++ src/Variables/Variables.java
@@ -0,0 +1,12 @@
+package Variables;
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+        int redApple = 11;
+
+        System.out.println("I have " + redApple + " red apples.");
+
+    }
+}
